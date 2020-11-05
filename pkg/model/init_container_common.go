d62dc068e07386c1b03ec84411ec73d898327deb
Rickard Östergård <rickard.ostergard@gmail.com>
2020-11-05 10:14:36 +0100

    add envar to init container

--- pkg/model/init_container_common.go
+++ pkg/model/init_container_common.go
@@ -7,17 +7,29 @@ import (
 	v1 "k8s.io/api/core/v1"
 )
 
+func getKeycloakExtensionsInitContainerEnv(cr *v1alpha1.Keycloak) []v1.EnvVar {
+	env := []v1.EnvVar{
+		{
+			Name:  KeycloakExtensionEnvVar,
+			Value: strings.Join(cr.Spec.Extensions, ","),
+		},
+	}
+
+	if len(cr.Spec.KeycloakDeploymentSpec.Experimental.Env) > 0 {
+		// We override Keycloak pre-defined envs with what user specified. Not the other way around.
+		env = MergeEnvs(cr.Spec.KeycloakDeploymentSpec.Experimental.Env, env)
+	}
+
+	return env
+}
+
 func KeycloakExtensionsInitContainers(cr *v1alpha1.Keycloak) []v1.Container {
+
 	return []v1.Container{
 		{
 			Name:  "extensions-init",
 			Image: Profiles.GetInitContainerImage(cr),
-			Env: []v1.EnvVar{
-				{
-					Name:  KeycloakExtensionEnvVar,
-					Value: strings.Join(cr.Spec.Extensions, ","),
-				},
-			},
+			Env:   getKeycloakExtensionsInitContainerEnv(cr),
 			VolumeMounts: []v1.VolumeMount{
 				{
 					Name:      "keycloak-extensions",
